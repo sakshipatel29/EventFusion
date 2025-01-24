991b763cd0f22dfd0df3230db239b7acb874739a
sakshipatel29 <sakshirpatel29062002@gmail.com>
2025-01-24 15:58:52 -0600

    Footer is ready to go with a link of homepage and copy rights.

--- event-app/components/shared/Footer.tsx
+++ event-app/components/shared/Footer.tsx
@@ -1,7 +1,17 @@
+import Image from "next/image";
+import Link from "next/link";
+
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="border-t">
+      <div className="flex-center wrapper flex-between flex flex-col p-5 gap-4 text-center sm:flex-row">
+        <Link href='/'>
+          <Image src='/assets/images/logo.svg' alt="logo" width={128} height={38}/>
+        </Link>
+        <p>2025 EventFusion. All rights reserved.</p>
+      </div>
+    </footer>
   )
 }
 
-export default Footer
+export default Footer;
