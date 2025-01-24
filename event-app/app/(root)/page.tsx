212b079458d9d92beffd015457511a1a878344e3
sakshipatel29 <sakshirpatel29062002@gmail.com>
2025-01-24 16:13:20 -0600

    Headlines and Explore Button are added on home page.

--- event-app/app/(root)/page.tsx
+++ event-app/app/(root)/page.tsx
@@ -1,9 +1,23 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main>
-    </main>
+    <>
+      <section className="bg-primary-50 bg-dotted-pattern bg-content py-5 md:py-10">
+        <div className="wrapper grid grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-0">
+          <div className="flex flex-col justify-center gap-8">
+            <h1 className="h1-bold">Host, Connect, Celebrate: Your Events, Our Platform!</h1>
+            <p className="p-regular-20 md:p-regular-24">Book and learn helpful tips from 3,168+ mentors in world-class companies with our global community.</p>
+            <Button size="lg" asChild className="button w-full sm:w-fit">
+              <Link href="#events">
+                Explore Now
+              </Link>
+            </Button>
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
